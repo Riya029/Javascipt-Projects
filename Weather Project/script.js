e7ced59593c83088b62cb587e169ed651cb31e32
Riya <riya198shrivastava@gmail.com>
2026-03-24 17:39:39 +0530

    Add event listener for search button

--- Weather Project/script.js
+++ Weather Project/script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "e2cd96aecda3077674f27a091c231e94"; // API key for OpenWeatherMap API
+const API_KEY = Write your api key; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { //HTML for the main weather card
